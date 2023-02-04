d4eeaf1eb7cdb3985cffb83dcd2220a1bd84ab19
islam-farahat <islam.mostafa.f@gmail.com>
2023-02-04 16:13:33 +0200

    update server

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/maintenance"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/maintenance/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(4200, "0.0.0.0");
